562d04226773346d5bc0cbadafc52a5572996ecb
Rupesh9369 <rupeshkumarsharma283@gmail.com>
2024-06-07 17:08:40 +0530

    updated

--- Section-3/Coding-Challenge-6.js
+++ Section-3/Coding-Challenge-6.js
@@ -1,28 +1,6 @@
 'use strict'
 console.log(`hey Coding-Challenge-6 `);
 
-// let price = prompt("Enter Price of your Bill : ");
-
-// let price1, price2;
-// if (price <= 300) {
-//   price1 = 15 / 100;
-//   price2 = price1 * price;
-//   console.log(
-//     `Your Bill Price is ${price} in this bill we add 15% tip price is ${price2} total bill is ${
-//       Number(price) + Number(price2)
-//     }`
-//   );
-// } else if (price >= 301) {
-//   price1 = 15 / 100;
-//   price2 = price1 * price;
-//   console.log(
-//     `Your Bill Price is ${price} in this bill we add 20% tip price is ${price2} total bill is ${
-//       Number(price) + Number(price2)
-//     }`
-//   );
-// } else {
-//   console.log(`WTF`);
-// }
 
 
 function tip() {
